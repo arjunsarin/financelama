175bdf9f9d382c8d4ec06b242de317bf069ef212
Arjun Sarin <arjun@sarin.de>
2019-10-29 10:52:17 +0100

    [Feature] Categorize transactions based on look-up table

--- financelama/core.py
+++ financelama/core.py
@@ -1,12 +1,36 @@
 # -*- coding: utf-8 -*-
+from typing import List
 
 import pandas as pd
+
+import financelama.lut_categories
 
 
 class Lama:
     data = pd.DataFrame(columns=[
         'day', 'info', 'orderer', 'orderer_account', 'orderer_bank',
-        'reason', 'value'])
+        'reason', 'value', 'category'])
+
+    def _get_category(self, identifier: str):
+        for category, keywords in financelama.lut_categories.categories.items():
+            # Check for each keyword
+            for k in keywords:
+                # Check if lower-case keyword is substring of lower-case identifier
+                if identifier.lower().find(k.lower()) != -1:
+                    return category
+        # Default value if no category was found
+        return 'other'
+
+
+    def categorize(self):
+        for index, row in self.data.iterrows():
+            concat = row['orderer'] + row['reason']
+            assigned_category = self._get_category(concat)
+            self.data.set_value(index, 'category', assigned_category)
+
+            # Print info message
+            info_str = row['orderer'] + '|' + row['info'] + '|' + row['reason']
+            print('[Categorize] TRANSACTION ' + info_str.ljust(80)[:80] + ' ASSIGNED TO ' + assigned_category)
 
     def cleanup(self):
         self.data = self.data.drop_duplicates()
---
+++ financelama/lut_categories.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+categories = {
+    'supermarket': ['rewe', 'coop', 'edeka', 'lidl', 'netto', 'norma', 'frukt', 'ica'],
+    'rent': ['miete' , 'miete', 'wohnung', 'etagenbeitrag'],
+    'entertainment': ['netflix', 'spotify', 'ticket', 'konzert', 'museum', 'filmstaden'],
+    'restaurant': ['restaurant', 'restaurant', 'bar', 'cafe', 'qstockholm'],
+    'traffic': ['db', 'deutschebahn', 'train', 'deutsche bahn'],
+}
