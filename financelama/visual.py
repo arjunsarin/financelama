9c658c8cccc0374f0740fc24f61c3b355e52b4e9
Arjun Sarin <arjun@sarin.de>
2020-02-25 23:15:24 +0100

    [visual] add pie charts

--- financelama/visual.py
+++ financelama/visual.py
@@ -3,11 +3,12 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
+import plotly.express as px
 
 import pandas as pd
 
 
-#ROADMAP Activity heatmap (github-like) see: https://community.plot.ly/t/colored-calendar-heatmap-in-dash/10907/5
+# ROADMAP Activity heatmap (github-like) see: https://community.plot.ly/t/colored-calendar-heatmap-in-dash/10907/5
 
 def generate_table(dataframe):
     return html.Table(
@@ -40,6 +41,7 @@ def generate_monthly_expenses(dataframe):
     # Sum up values per week, returns object with DatetimeIndex
     extract = extract.groupby(pd.Grouper(key='day', freq='M')).sum()
 
+    # Create diagram and fill with data
     data = [dict(
         type='bar',
         x=extract.index,
@@ -52,6 +54,18 @@ def generate_monthly_expenses(dataframe):
     )
 
     return dcc.Graph(figure=go.Figure(data, layout))
+
+
+def generate_pie_chart_expenses(dataframe):
+    df = dataframe.loc[dataframe['value'] < 0].copy()
+    df['value'] = df['value'].abs()
+
+    return dcc.Graph(figure=px.pie(df, values='value', names='category', title='Expenses'))
+
+
+def generate_pie_chart_income(dataframe):
+    df = dataframe.loc[dataframe['value'] > 0].copy()
+    return dcc.Graph(figure=px.pie(df, values='value', names='category', title='Income'))
 
 
 def start_dashboard(dataframe: pd.DataFrame):
@@ -67,10 +81,21 @@ def start_dashboard(dataframe: pd.DataFrame):
     app = dash.Dash(__name__, external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
 
     app.layout = html.Div(children=[
-        html.H1(children='Financelama'),
+        html.H1('Financelama'),
 
         # generate_table(lama.data)
-        generate_monthly_expenses(dataframe)
+        generate_monthly_expenses(dataframe),
+
+        html.Div([
+            html.Div([
+                generate_pie_chart_expenses(dataframe)
+            ], className="six columns"),
+
+            html.Div([
+                generate_pie_chart_income(dataframe)
+            ], className="six columns")
+        ], className="row")
+
     ])
 
     app.run_server(debug=True)
--- tests/basic_function.py
+++ tests/basic_function.py
@@ -4,10 +4,13 @@ import timeit
 
 from financelama.file_import import read_file_dkb, read_folder_dkb, read_file_paypal
 from financelama.process import categorize, modify_report
+from financelama.evaluation import evaluate
+from financelama.visual import start_dashboard
 
 
 def benchmark(function):
     print(timeit.timeit(function, number=1))
+
 
 # Create lama object with dataframe as data property
 lama = Financelama()
@@ -15,17 +18,16 @@ lama = Financelama()
 # Read files
 # obj.read_file('E:/Dokumente/Finanzen/Analyse/financelama-git/data/1051054540(3).csv')
 # Lots of data here
-read_folder_dkb(lama, 'data/dkb/')
+# read_folder_dkb(lama, 'data/dkb/')
 # read_file_paypal(lama, 'E:/Dokumente/Finanzen/Analyse/financelama-git/data/paypal_download.CSV')
 
-categorize(lama)
+# categorize(lama)
 
-modify_report(lama, 'urlaub', list_of_rowids={1, 2, 3})
-modify_report(lama, 'urlaub', list_of_ranges={(617, 620)})
+# modify_report(lama, 'urlaub', list_of_rowids={1, 2, 3})
+# modify_report(lama, 'urlaub', list_of_ranges={(617, 620)})
 
-#evaluate()
 
 # Start DASH web application
-#visual.start_dashboard(lama.connect_database()[0])
+start_dashboard(evaluate(lama))
 
-#os.remove('lama.db')
+# os.remove('lama.db')
